b68541279d625c0d9479f9cc478344088a985f70
Keith Mahoney <kmahone@microsoft.com>
2019-01-15 16:34:51 -0800

    run tests on rs2 and rs3 with extra appx logging

--- build/Helix/runtests.cmd
+++ build/Helix/runtests.cmd
@@ -1,15 +1,19 @@
 robocopy %HELIX_CORRELATION_PAYLOAD% . /s /NP
 
-dir /b /s
+te MUXControls.Test.dll MUXControlsTestApp.appx IXMPTestApp.appx /enablewttlogging /unicodeOutput:false /sessionTimeout:0:15 /testtimeout:0:10 /screenCaptureOnError %* 
+set taeferrorlevel = %errorlevel%
 
-reg add HKLM\Software\ElevateIfNeeded /f 
-if not errorlevel 1 (
-    reg delete HKLM\Software\ElevateIfNeeded /f >nul
-    echo elevated
-) else (
-    echo not elevated
+%HELIX_PYTHONPATH% %HELIX_SCRIPT_ROOT%\upload_result.py -result te.wtl -result_name te.wtl
+
+FOR %%I in (WexLogFileOutput\*.jpg) DO (
+    echo Uploading %%I to "%HELIX_RESULTS_CONTAINER_URI%/%%~nI%%~xI%HELIX_RESULTS_CONTAINER_RSAS%"
+    %HELIX_PYTHONPATH% %HELIX_SCRIPT_ROOT%\upload_result.py -result %%I -result_name %%~nI%%~xI 
 )
 
+if %taeferrorlevel% == 0 goto uploadlogs
+
+
+REM Upload appx logs
 cd scripts
 TDRDump.exe > tdrdump.txt
 echo Uploading tdrdump.txt to "%HELIX_RESULTS_CONTAINER_URI%/tdrdump.txt%HELIX_RESULTS_CONTAINER_RSAS%"
@@ -20,25 +24,13 @@ cd scripts
 powershell -ExecutionPolicy Bypass .\runappxdiag.ps1
 cd ..
 
-REM take a screenshot:
-te MUXControls.Test.dll /unicodeOutput:false /screenCaptureOnError /name:ffddfdfdf
-
-powershell -ExecutionPolicy Bypass -Command "Wait-Process AppxDiag -Timeout 120"
-
 FOR %%I in (scripts\*.zip) DO (
     echo Uploading %%I to "%HELIX_RESULTS_CONTAINER_URI%/%%~nI%%~xI%HELIX_RESULTS_CONTAINER_RSAS%"
     %HELIX_PYTHONPATH% %HELIX_SCRIPT_ROOT%\upload_result.py -result %%I -result_name %%~nI%%~xI 
 )
 
-REM te MUXControls.Test.dll MUXControlsTestApp.appx IXMPTestApp.appx /enablewttlogging /unicodeOutput:false /sessionTimeout:0:15 /testtimeout:0:10 /screenCaptureOnError %* 
 
-REM %HELIX_PYTHONPATH% %HELIX_SCRIPT_ROOT%\upload_result.py -result te.wtl -result_name te.wtl
-
-FOR %%I in (WexLogFileOutput\*.jpg) DO (
-    echo Uploading %%I to "%HELIX_RESULTS_CONTAINER_URI%/%%~nI%%~xI%HELIX_RESULTS_CONTAINER_RSAS%"
-    %HELIX_PYTHONPATH% %HELIX_SCRIPT_ROOT%\upload_result.py -result %%I -result_name %%~nI%%~xI 
-)
-
+:uploadlogs
 cd scripts
 powershell -ExecutionPolicy Bypass .\ConvertWttLogToXUnit.ps1 ..\te.wtl ..\testResults.xml %testnameprefix%
 cd ..
